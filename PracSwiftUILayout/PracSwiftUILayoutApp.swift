d83d467de502f2a380afef4147e29dc3bf780682
신의연 <suy@sin-uiyeon-ui-MacBookPro-2.local>
2022-09-22 10:34:51 +0900

    [chore] initial commit

---
+++ PracSwiftUILayout/PracSwiftUILayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracSwiftUILayoutApp.swift
+//  PracSwiftUILayout
+//
+//  Created by 신의연 on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PracSwiftUILayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
