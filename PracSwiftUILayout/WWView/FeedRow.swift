beaff93d4bd80c8ca60ef05bfe1d2a5cb8441c6b
신의연 <suy@sin-uiyeon-ui-MacBookPro-2.local>
2022-09-22 14:57:00 +0900

    [feat] FeedRow 구현

--- PracSwiftUILayout/WWView/FeedRow.swift
+++ PracSwiftUILayout/WWView/FeedRow.swift
@@ -9,23 +9,37 @@ import SwiftUI
 
 struct FeedRow: View {
     var body: some View {
-        HStack {
-            CircleImage()
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("name1234asdf")
-                    Spacer()
-                        .font(.headline)
-                    Text("오후 01:20")
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-                
-                Text("미사 롯데캐슬스타")
+        VStack {
+            
+            UserInfoView(userInfo: UserInfo(id: "id1", address: "미사 롯데캐슬스타", imageName: ""))
+            HStack {
+                Text("name123513251235123님과 과자구매 어쩌구 저쩌구 testtest 거래가 이루어졌어요")
+                    .font(.headline)
+                Spacer()
+            }
+            
+            HStack {
+                Text("거래완료")
                     .font(.subheadline)
+                    .background(Color(.green))
+                    .padding(EdgeInsets(top: 3, leading: 3, bottom: 3, trailing: 3))
+                Text("5000원")
+                Spacer()
+            }
+            
+            Divider()
+            
+            HStack {
+                Image(systemName: "heart")
+                    .foregroundColor(.red)
+                Text("1")
+                Spacer()
+                Text("과자구매 (건대입구 1238번출구 사거리세곔ㄴㅇㄹㅁㄴㅇㄹㅁㅇ")
+                    .lineLimit(1)
             }
         }
         .padding()
+        
     }
 }
 
